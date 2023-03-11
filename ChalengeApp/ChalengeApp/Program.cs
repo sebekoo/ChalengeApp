5dbd903ee1083750184b7a9b25fe3d6e46785cd0
sebekoo <sebekoo@o2.pl>
2023-03-11 22:05:05 +0100

    Praca domowa dzień 4

--- ChalengeApp/ChalengeApp/Program.cs
+++ ChalengeApp/ChalengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Cześć, witam w trzecim dniu, seba!");
+﻿string myName = "Ewa";
+char mySex = 'k';
+int myAge = 37;
+
+if(myName == "Ewa" && myAge == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if(myAge < 30 && mySex == 'k')
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(myAge < 18 && mySex != 'k')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna"); 
+}
+else
+{
+    Console.WriteLine(myName + ", mam " + myAge + " lat");
+}
